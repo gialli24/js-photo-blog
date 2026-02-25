a10efb525df85fd7b4a869270a250df2c037ef38
gialli24 <a.gialli2409@gmail.com>
2026-02-25 11:24:59 +0100

    milestone 2 - test api on postman and write result in main js comment

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/* 
+    endpoint: https://lanciweb.github.io/demo/api/pictures/
+    response:
+    [
+        {
+            "id": 1,
+            "title": "Skate Park",
+            "date": "01-07-2024",
+            "url": "https://marcolanci.it/boolean/assets/pictures/1.png"
+        },
+        {
+            "id": 2,
+            "title": "Passeggiata",
+            "date": "16-07-2024",
+            "url": "https://marcolanci.it/boolean/assets/pictures/2.png"
+        },
+        ...
+    ]
+
+    Array of objects with id, title, date, url properties
+*/
